af3cac8f848c494ea5a4c102f89e4c9f47e2b9f6
Karan singh bisht <95972127+xander-wild@users.noreply.github.com>
2021-12-30 18:43:08 +0530

    Add files via upload
    Python Program to check Armstrong Number
    Difficulty Level : Medium
    Last Updated : 12 Dec 2021
    Given a number x, determine whether the given number is Armstrong number or not. A positive integer of n digits is called an Armstrong number of order n (order is number of digits) if

---
+++ armstrongnumber.py
@@ -0,0 +1,22 @@
+#Python Program to check Armstrong Number
+#Difficulty Level : Medium
+#Last Updated : 12 Aug, 2020
+#Given a number x, determine whether the given number
+#  is Armstrong number or not. A positive integer of n
+#  digits is called an Armstrong number of order n
+#  (order is number of digits) if.
+
+#abcd... = pow(a,n) + pow(b,n) + pow(c,n) + pow(d,n) + .... 
+
+#153= pow(1,3)+pow(5,3)+pow(3,3)
+i = int(input (" enter the number:"))
+orig=i
+sum=0
+while (i>0) :
+    sum =sum + (i%10)*(i%10)*(i%10)
+    i=i//10
+
+if orig==sum:
+    print("  armstrong")
+else :
+    print(" not armstrong")
